a00950407e55c9cfa7e42a7441157d506977196d
shdboom <928421021@qq.com>
2021-12-02 11:36:19 +0800

    接口服务页面优化

--- pages/product/interface/index.vue
+++ pages/product/interface/index.vue
@@ -41,20 +41,43 @@
         </li>
       </ul>
     </div>
-    <div class="pd-100 bgfa nbottom" style="margin-bottom: 100px;">
-      <div class="title">统一的API服务管理</div>
-      <div class="description">实时反馈APP与API的交互情况，请求参数，返回结果，瞬间重现问题场景 <br>
-        实现发现问题，快速解决问题
-      </div>
-      <div class="oneimg">
-        <img src="/images/info/api_img.png">
-      </div>
+    <div class="pd-100" style="margin-bottom: 100px;background-color: #f8f8f8">
+
+      <div class="title">我们的优势</div>
+      <ul class="content icon-dispaly">
+        <li>
+          <img src="https://qiniu.easyapi.com/%E5%BD%A2%E7%8A%B6%2023.png">
+          <span>专业技术</span>
+          <p>团队专注于API领域服务多年经验</p>
+        </li>
+        <li>
+          <img src="https://qiniu.easyapi.com/%E5%BD%A2%E7%8A%B6%2024.png">
+          <span>性能稳定</span>
+          <p>通过自研的EasyAPI网关系统性能无比可靠</p>
+        </li>
+        <li>
+          <img src="https://qiniu.easyapi.com/%E5%BD%A2%E7%8A%B6%2025.png">
+          <span>私有化部署</span>
+          <p>基于API的服务管理中心可进行私有化部署</p>
+        </li>
+        <li>
+          <img src="https://qiniu.easyapi.com/%E5%BD%A2%E7%8A%B6%2026.png">
+          <span>需求定制</span>
+          <p>满足不同客户的更深层次需求可实现绣球自定义</p>
+        </li>
+      </ul>
+      <!--<div class="description">实时反馈APP与API的交互情况，请求参数，返回结果，瞬间重现问题场景 <br>-->
+      <!--实现发现问题，快速解决问题-->
+      <!--</div>-->
+      <!--<div class="oneimg">-->
+      <!--<img src="/images/info/api_img.png">-->
+      <!--</div>-->
     </div>
   </div>
 </template>
 
 <script>
-import Index from './index'
+  import Index from './index'
 
-export default Index
+  export default Index
 </script>
