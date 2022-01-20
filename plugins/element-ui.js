6d1c0f56de7b79fcb3d016628821d7972bace2b5
leida <zhang20084@126.com>
2022-01-20 22:09:46 +0800

    fix:element按需引用

--- plugins/element-ui.js
+++ plugins/element-ui.js
@@ -1,4 +1,25 @@
-import Vue from 'vue'
-import Element from 'element-ui'
+import Vue from 'vue';
+import {
+  Dialog,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+  Popover,
+  Empty,
+  Loading,
+  Message,
+} from 'element-ui';
 
-Vue.use(Element)
+Vue.use(Dialog);
+Vue.use(Dropdown);
+Vue.use(DropdownMenu);
+Vue.use(DropdownItem);
+Vue.use(Button);
+Vue.use(Popover);
+Vue.use(Empty);
+
+Vue.use(Loading.directive);
+
+Vue.prototype.$loading = Loading.service;
+Vue.prototype.$message = Message;
