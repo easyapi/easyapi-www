5646df616eb2bde8e814766651fe70e11575932b
leida <zhang20084@126.com>
2022-03-04 22:34:54 +0800

    fix:样式表整理

--- pages/post/_id.vue
+++ pages/post/_id.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="mg-t-72">
-    <div class='container pd-t-20'>
-      <div class='detail-wrapper'>
-        <div class='detail-title'>
+    <div class='container'>
+      <div class='post-detail'>
+        <div class='post-title'>
           <span>{{ article.title }}</span>
-          <label class='time'>{{ article.updateTime.split(' ')[0] }}</label>
+          <label class='post-title_time'>{{ article.updateTime.split(' ')[0] }}</label>
         </div>
-        <div class='detail-content' v-html='article.content'>
-        </div>
+        <div class='post-content' v-html='article.content'></div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  import Id from './_id'
+import Id from './_id'
 
-  export default Id
+export default Id
 </script>
