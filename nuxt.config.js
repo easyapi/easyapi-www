1fe02a0f19b66b6d1adcd745709dc8ca139fba8e
chenyun123 <1537299563@qq.com>
2022-03-11 15:42:40 +0800

    集成tailwindcss

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,43 +1,39 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: '{{ name }} - EasyAPI',
     meta: [
-      {charset: 'utf-8'},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: '{{escape description }}'}
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '{{escape description }}' }
     ],
-    link: [
-      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
-    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
-  css: [
-    '@/assets/scss/base.scss',
-    '@/assets/scss/element-variables.scss'
-  ],
-  plugins: [
-    '@/plugins/axios',
-    '@/plugins/element-ui'
-  ],
+  css: ['@/assets/scss/base.scss', '@/assets/scss/element-variables.scss'],
+  plugins: ['@/plugins/axios', '@/plugins/element-ui'],
   /*
-  ** Customize the progress bar color
-  */
-  modules: [
-    '@nuxtjs/axios'
-  ],
+   ** Customize the progress bar color
+   */
+  modules: ['@nuxtjs/axios'],
+  buildModules: ['@nuxtjs/tailwindcss'],
+  tailwindcss: {
+    cssPath: '@/assets/css/tailwind.css',
+    configPath: 'tailwind.config.js',
+    exposeConfig: false,
+    config: {}
+  },
   loading: {
     color: '#3B8070'
   },
   target: 'static',
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     transpile: [/^element-ui/],
-    extend(config, ctx) {
-    },
+    extend(config, ctx) {},
     maxChunkSize: 300000, // 单个包最大尺寸
     extractCSS: true, // 单独提取CSS
     babel: {
