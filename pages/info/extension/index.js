42a593bfc6269f75382749c9a7fdc8d2f6879f27
leida <zhang20084@126.com>
2022-03-23 15:57:08 +0800

    fix:浏览器插件优化

--- pages/info/extension/index.js
+++ pages/info/extension/index.js
@@ -4,7 +4,6 @@ import Pay from "../../../components/pay";
 
 export default {
   name: 'InfoExtension',
-  components: {Pay},
   head() {
     return {
       title: '浏览器插件 - EasyAPI服务平台',
@@ -14,20 +13,15 @@ export default {
       ]
     }
   },
-  data() {
-    return {
-      num: ""
-    }
-  },
   methods: {
-    showCode() {
-      this.$refs.child.dialogVisible = true
-    },
     jumpGoogle() {
-      this.num = 1
+      this.$message.warning("谷歌浏览器插件正在审核中")
     },
     jumpFirefox() {
-      this.num = 2
+      window.open("https://addons.mozilla.org/zh-CN/firefox/addon/easyapi%E6%B5%8F%E8%A7%88%E5%99%A8%E6%89%A9%E5%B1%95/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search","_blank");
+    },
+    jumpZip() {
+      window.open("https://qiniu.easyapi.com/easyapi-extension.zip","_blank");
     }
   }
 }
--- pages/info/extension/index.vue
+++ pages/info/extension/index.vue
@@ -6,9 +6,8 @@
         <p class="text-center text-gray-400 pb-12">EasyAPI量身定制的Chrome、firefox插件，可以快速进行接口编辑、测试等接口工作</p>
         <div class="w-2/5 p-12 bg-white shadow-md border-solid border rounded mx-auto">
           <p class="text-2xl py-6 text-center  col-18C1D6 bg-CBEFF3">最新版本：v0.1,更新内容</p>
-          <div :class="{active:num==1}" @click="jumpGoogle"
-               class="cursor-pointer flex border-solid border w-60 p-1 mt-12 mx-auto">
-            <img src="../../../assets/images/google.png">
+          <div @click="jumpGoogle" class="cursor-pointer flex border-solid border w-60 p-1 mt-12 mx-auto">
+            <img src="../../../assets/images/google.png" alt="Google浏览器">
             <div class="ml-0.5">
               <p class="leading-6 font-normal">Available in the</p>
               <p class="leading-6 font-medium">Chrome Web Store</p>
@@ -18,22 +17,18 @@
             <p class="text-center">推荐</p>
             <p class="text-center text-gray-400">（需要代理）</p>
           </div>
-          <div :class="{active:num==2}"  @click="jumpFirefox" class="flex cursor-pointer border-solid border w-48 p-1 mt-12 mx-auto">
-            <img src="../../../assets/images/firefox.png">
-            <div class="ml-1.5">
-              <p class="leading-6 font-normal">GET THE</p>
-              <p class="leading-6 font-medium text-2xl">ADD-ON</p>
-            </div>
+          <div @click="jumpFirefox" class="flex cursor-pointer border-solid border w-48 p-1 mt-12 mx-auto">
+            <img src="https://qiniu.easyapi.com/firefox-addons.svg" alt="火狐浏览器">
           </div>
           <div class="my-6">
             <p class="text-center">推荐</p>
           </div>
           <div class="flex justify-center">
-            <el-button type="primary" round>本地安装</el-button>
+            <el-button type="primary" round @click="jumpZip">本地安装</el-button>
           </div>
           <p class="mt-4 text-center">推荐，针对国内无法访问Chrome商店的用户，各种Chrome内核浏览器兼容</p>
           <p class="mt-4 text-center col-18C1D6 underline cursor-pointer">本地安装教程</p>
-          <p class="mt-4 text-center">*本地安装特别注意需要开启浏览器的<span class="col-18C1D6 cursor-pointer">开发者模式</span></p>
+          <p class="mt-4 text-center">*本地安装特别注意需要开启浏览器的<span class="col-18C1D6">开发者模式</span></p>
         </div>
       </div>
     </div>
@@ -41,7 +36,7 @@
 </template>
 
 <script>
-  import Index from './index'
+import Index from './index'
 
-  export default Index
+export default Index
 </script>
