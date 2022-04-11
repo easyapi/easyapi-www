ab7f621c9a485e717484e09a503afaaa0559f7aa
leida <zhang20084@126.com>
2022-04-11 21:35:16 +0800

    fix:优化退出

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,7 +2,6 @@ import Cookies from 'js-cookie'
 
 export default function ({$axios, redirect}) {
   $axios.onRequest((config) => {
-    Cookies.set('authenticationToken', 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ6aGFuZzIwMDg0QDEyNi5jb20iLCJhdXRoIjoiUk9MRV9BRE1JTiIsImV4cCI6MTY0ODg2MDMxMn0.5UqyBTbc_5XDlMv3aGoMcqsJj2Wbbno0q136dRk9lAxanD4bTxFtXpy85DOqdo3dtG076zo2-6jEZwHaHamFng')
     if (Cookies.get('authenticationToken')) {
       config.headers.Authorization = 'Bearer ' + Cookies.get('authenticationToken')
     }
--- store/modules/user.js
+++ store/modules/user.js
@@ -69,6 +69,7 @@ const user = {
         }
       }).catch(error => {
         Cookies.remove('authenticationToken')
+        Cookies.remove('authenticationToken', {path: '/', domain: '.easyapi.com'})
       })
     },
     /**
@@ -76,6 +77,7 @@ const user = {
      */
     logout() {
       Cookies.remove('authenticationToken')
+      Cookies.remove('authenticationToken', {path: '/', domain: '.easyapi.com'})
     }
   }
 }
