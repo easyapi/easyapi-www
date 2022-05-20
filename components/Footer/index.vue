0679aecabb700105d86d2d3441f827386b8fc961
leida <zhang20084@126.com>
2022-05-20 23:30:54 +0800

    fix:链接完善

--- components/Footer/index.vue
+++ components/Footer/index.vue
@@ -4,7 +4,7 @@
       <div class="footer-item flex justify-between mg-15-auto">
         <div class="w-3/12">
           <img class="w-40" src="https://qiniu.easyapi.com/market/logo.svg">
-          <p class="w-4/5 mt-6">
+          <p class="w-4/5 mt-6 text-xl">
             新一代API接口管理平台，EasyAPI是一个为您提供API管理、API测试、API监控、API文档、API低代码服务的综合性API服务平台
           </p>
           <p>联系电话：<span class="color-00AEC5">136-5617-1020</span></p>
@@ -16,9 +16,9 @@
               <img src="https://qiniu.easyapi.com/weixin-easyapi.jpg">
               <a slot="reference"><img class="w-12 mr-4" src="../../assets/images/wechat.svg" alt=""></a>
             </el-popover>
-
-            <a href="https://github.com/easyapi" target="_blank"><img class="w-12" src="../../assets/images/github.svg"
-                                                                      alt=""></a>
+            <a href="https://github.com/easyapi" target="_blank">
+              <img class="w-12" src="../../assets/images/github.svg" alt="">
+            </a>
           </div>
         </div>
         <div class="w-6/12 flex justify-around">
@@ -46,13 +46,13 @@
           </div>
           <div class="flex flex-col">
             <h2>关于</h2>
-            <a>关于我们</a>
-            <a>合作伙伴</a>
-            <a>联系我们</a>
-            <a>支持我们</a>
+            <a href="/info/about">关于我们</a>
+            <a href="/info/together">合作伙伴</a>
+            <a href="/info/contact">联系我们</a>
+            <a href="/info/support">支持我们</a>
           </div>
         </div>
-        <div class="w-2/12 flex justify-end text-c">
+        <div class="w-2/12 flex justify-end text-center">
           <div class="mr-12">
             <img class="w-32" src="https://qiniu.easyapi.com/qrcode/leida.png" alt="EasyAPI技术咨询">
             <p class="mt-5">技术咨询</p>
@@ -64,7 +64,7 @@
         </div>
       </div>
       <el-divider class="mg-15-auto"></el-divider>
-      <div class="mg-15-auto text-c">
+      <div class="mg-15-auto text-center">
         <a href="https://beian.miit.gov.cn/" target="_blank">Copyright © 2015～2022 无锡帮趣数据服务有限公司</a>
       </div>
     </div>
@@ -72,50 +72,46 @@
 </template>
 
 <script>
-  export default {
-    name: 'Footer'
-  }
+export default {
+  name: 'Footer'
+}
 </script>
 
 <style scoped lang="scss">
-  .footer {
-    background: #3C3C3C;
+.footer {
+  background: #3C3C3C;
+  color: gray;
+  padding: 20px 0;
+  font-size: 1.2rem;
+
+  h2 {
+    font-size: 16px;
+    margin-bottom: 10px;
+  }
+
+  p {
+    margin-bottom: 10px;
+  }
+
+  a {
     color: gray;
-    padding: 20px 0;
-    font-size: 1.2rem;
+    margin-bottom: 10px;
 
-    h2 {
-      font-size: 16px;
-      margin-bottom: 10px;
-    }
-
-    p {
-      margin-bottom: 10px;
-    }
-
-    a {
-      color: gray;
-      margin-bottom: 10px;
-
-      &:hover {
-        color: #fff;
-      }
+    &:hover {
+      color: #fff;
     }
   }
+}
 
-  .mg-15-auto {
-    margin: 15px auto;
-  }
+.mg-15-auto {
+  margin: 15px auto;
+}
 
-  .color-00AEC5 {
-    color: #00AEC5;
-  }
+.color-00AEC5 {
+  color: #00AEC5;
+}
 
-  .text-c {
-    text-align: center;
-  }
-
-  .el-divider {
-    background-color: #606266;
-  }
+.el-divider {
+  background-color: #606266;
+}
 </style>
