11d2c5fc687bd64b6ded16445a8746af57403ee6
shdboom <928421021@qq.com>
2022-07-06 18:17:21 +0800

    判断是否登录跳转team

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,29 @@
 <template>
-  <div>
-    <Header/>
-    <nuxt/>
-    <Footer/>
-  </div>
+    <div>
+        <Header/>
+        <nuxt/>
+        <Footer/>
+    </div>
 </template>
 <script>
-import Header from '../components/Header/index.vue'
-import Footer from '../components/Footer/index.vue'
+    import Header from '../components/Header/index.vue'
+    import Footer from '../components/Footer/index.vue'
+    import Cookies from 'js-cookie'
 
-export default {
-  components: {
-    Header,
-    Footer
-  }
-};
+    export default {
+        components: {
+            Header,
+            Footer
+        },
+        mounted() {
+            console.log($nuxt.$route.path)
+            if ($nuxt.$route.path == '/') {
+                if (Cookies.get('authenticationToken')) {
+                    window.location.href = 'https://team.easyapi.com/'
+                }
+            }
+        }
+    };
 </script>
 <style>
 
