946c90fd911d61bff6ab9f324d909d08bb3afcd7
leida <zhang20084@126.com>
2022-07-27 11:41:11 +0800

    fix:团队文案修改

--- pages/info/about/index.js
+++ pages/info/about/index.js
@@ -8,7 +8,7 @@ export default {
       title: '关于我们 - EasyAPI服务平台',
       meta: [
         { hid: 'description', name: 'description', content: 'EasyAPI关于我们' },
-        { hid: 'keyword', name: 'keyword', content: '关于我们' }
+        { hid: 'keyword', name: 'keyword', content: '关于我们,EasyAPI团队成员,EasyAPI团队介绍,EasyAPI公司介绍' }
       ]
     }
   }
