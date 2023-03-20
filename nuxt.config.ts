ededdfdf475a17a5aedcf456874bfca84df55b6d
leida <zhang20084@126.com>
2023-03-20 23:43:23 +0800

    build:element-plus引用改成官方推荐写法

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,8 @@
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-
 const lifecycle = process.env.npm_lifecycle_event
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss',
+  modules: ['@nuxtjs/tailwindcss', '@element-plus/nuxt',
     ['@pinia/nuxt', {
       autoImports: [
       // automatically imports `defineStore`
@@ -27,19 +23,6 @@ export default defineNuxtConfig({
   ],
   components: true,
   vite: {
-    plugins: [
-      AutoImport({
-        resolvers: [ElementPlusResolver()],
-      }),
-      Components({
-        dts: true,
-        resolvers: [
-          ElementPlusResolver({
-            importStyle: false,
-          }),
-        ],
-      }),
-    ],
     build: {
       chunkSizeWarningLimit: 4096,
       rollupOptions: {
