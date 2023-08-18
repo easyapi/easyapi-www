d5a1b5efec447f10719ceecf7c5bab7084c06127
leida <zhang20084@126.com>
2023-08-18 10:29:08 +0800

    fix:命名错误修改

--- utils/token.ts
+++ utils/token.ts
@@ -5,14 +5,14 @@ const key = 'authenticationToken'
 /**
  * 获取token
  */
-export function getToken() {
-  return useCookie(key).value
+export function getToken(): string | undefined {
+  return useCookie(key).value as string | undefined
 }
 
 /**
  * 移除token
  */
-export function removeToken() {
+export function removeToken(): void {
   useCookie(key).value = null
   useCookie(key, { path: '/', domain: '.easyapi.com' }).value = null
 }
